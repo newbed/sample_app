1c33c13f7cce819bdc0f33831efae8a7a0e50f23
Dave Newberry <david.newberry@gmail.com>
2011-03-28 09:01:20 -0400

    Improved the user show page and added a gravatar

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,13 +10,28 @@ describe UsersController do
     end
     
     it "should be successful" do
-      get :show, :id => @user.id
+      get :show, :id => @user.id  # don't need the .id (rails is permissive)
       response.should be_success
     end
     
     it "should find the right user" do
       get :show, :id => @user.id
       assigns(:user).should == @user #NB... 1:13 lesson 7 video
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user.id
+      response.should have_selector('title', :content => @user.name)
+    end
+    
+    it "should have the user's name" do
+      get :show, :id  => @user.id
+      response.should have_selector('h1', :content => @user.name)
+    end
+    
+    it "should have profile image (globally recognized...) image" do
+      get :show, :id  => @user.id
+      response.should have_selector('h1>img', :class => 'gravatar')
     end
     
   end

aacb12ab7be5ee0e568c65fdc2d3263f91187017
Dave Newberry <david.newberry@gmail.com>
2011-04-08 08:36:29 -0400

    handling invalid signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
   
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
   
   get "pages/home"
   get "pages/contact"

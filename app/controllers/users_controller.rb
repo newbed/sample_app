6f13b856075a18d8af96abe5b2052743b8c79d2b
Dave Newberry <david.newberry@gmail.com>
2011-04-10 16:17:11 -0400

    working index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   #authenticate method for the before_filter method
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:edit, :update, :index]
   before_filter :correct_user, :only => [:edit, :update]
+  
+  def index
+    @title = "All users"
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
